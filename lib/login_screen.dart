b7aeef08b6bcf768324901720cc1b6b4fb8b9767
stc <sanjarchikchaturjonov@gmail.com>
2023-11-15 09:44:20 +0000

    first_commit

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<LoginScreen> {
+  bool isLoggedIn = false;
+
+  void _changeIcon() {
+    setState(() {
+      isLoggedIn = !isLoggedIn;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,10 +23,15 @@ class LoginScreen extends StatelessWidget {
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                  Icon(
-                    Icons.lock_outline,
-                    size: 100,
-                  ),
+                  isLoggedIn
+                      ? Icon(
+                          Icons.login,
+                          size: 100,
+                        )
+                      : Icon(
+                          Icons.lock_outline,
+                          size: 100,
+                        ),
                   SizedBox(height: 16),
                   Text(
                     'Login',
@@ -26,7 +43,9 @@ class LoginScreen extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
@@ -46,35 +65,44 @@ class LoginScreen extends StatelessWidget {
                     ),
                     SizedBox(height: 50),
                     ElevatedButton(
-
-                      onPressed: () {},
-                      child: Text('Login'),
+                      onPressed: () {
+                        _changeIcon();
+                      },
+                      child: Text(isLoggedIn ? 'Logged in' : 'Login'),
                       style: ElevatedButton.styleFrom(
                           primary: Colors.black,
-                          fixedSize: const Size(300, 50)
-                      ),
-
+                          fixedSize: const Size(300, 50)),
                     ),
                     SizedBox(height: 50),
                     Center(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Image.asset(
-                            'images/insta.png',
-                            height: 100,
-                            width: 100,
-                          ),
-                          Image.asset(
-                            'images/meta1.png',
-                            height: 100,
-                            width: 100,
-                          ),
+                          isLoggedIn
+                              ? Image.asset(
+                                  'images/insta.png',
+                                  height: 100,
+                                  width: 40,
+                                )
+                              : Image.asset(
+                                  'images/meta1.png',
+                                  height: 100,
+                                  width: 100,
+                                ),
+                          isLoggedIn
+                              ? Image.asset(
+                                  'images/meta1.png',
+                                  height: 100,
+                                  width: 100,
+                                )
+                              : Image.asset(
+                                  'images/insta.png',
+                                  height: 100,
+                                  width: 40,
+                                ),
                         ],
                       ),
                     ),
-
-
                   ],
                 ),
               ),
